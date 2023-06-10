ce7dfba9f775a7e018d6a606172afda116174138
郑相关 <codingdogzxg@gmail.com>
2023-06-10 22:31:41 +0800

    Update build-win64.bat

--- build/build-win64.bat
+++ build/build-win64.bat
@@ -1,3 +1,7 @@
 pyinstaller -F --clean ../main.py -p ../core/ExcelSerializer.py -p ../core/Utility -p ../core/MicroblogCrawler.py
+
+if exist dist\MicroblogCrawler.exe (
+    del dist\MicroblogCrawler.exe
+)
 ren dist\main.exe MicroblogCrawler.exe
 pause
